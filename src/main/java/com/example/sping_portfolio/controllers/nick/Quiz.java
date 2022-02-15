9a76665848fe00204d1b61c68da19994602a76d9
ncrxue <nxue2004@gmail.com>
2022-02-15 10:19:28 -0800

    pop quiz

---
+++ src/main/java/com/example/sping_portfolio/controllers/nick/Quiz.java
@@ -0,0 +1,34 @@
+package com.example.sping_portfolio.controllers.nick;
+
+import java.util.ArrayList;
+
+// Write a Class Number
+public class Quiz {
+    // instance variables
+
+    // Number has a zero Argument constructor
+    // It initializes a random number between 3 and 36, ie the number of squirrels in class
+    public Quiz() {
+        // constructor
+    }
+
+    // It contains a getter for the Random Number
+
+
+    // It contains a getter for Index, or the order it was initialized
+
+
+    // Write a tester method
+    public static void main(String[] args) {
+
+        // Create an ArrayList of Type Number, my ArrayList is called squirrels
+
+        // Initialize 10 squirrels of class type Number
+        // Insert Number instance into ArrayList Squirrel in least to greatest order by random number, mine required nested loops
+
+
+        // Print a formatted message with number of Squirrels and Index by which they were created, use enhanced for loop
+
+    }
+
+}

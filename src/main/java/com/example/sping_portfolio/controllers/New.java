17d0f61b064c6e048f28c3624912261697dbe1ef
gracele246 <73254122+gracele246@users.noreply.github.com>
2022-02-28 00:58:20 -0800

    working project

--- src/main/java/com/example/sping_portfolio/controllers/New.java
+++ src/main/java/com/example/sping_portfolio/controllers/New.java
@@ -1,13 +1,27 @@
 package com.example.sping_portfolio.controllers;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 
 @Controller
 public class New {
     @GetMapping("/new")
-    public String New(Model model){
-        return "newReleases";
+    public String New(Model model) {
+
+            return "helloWorld";
     }
 }
+
